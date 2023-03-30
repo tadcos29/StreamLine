76e0e7ebc6e9e839b11bb6ecfc9ab193a6687ad9
m1taylor <m1taylor@ryerson.ca>
2023-03-30 19:11:15 -0400

    added nav

--- client/src/components/user/Header.js
+++ client/src/components/user/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Auth from "../../utils/auth";
 import UserNav from "./UserNav";
+import "./user.css";
+
 // import { Link } from "react-router-dom";
 // import { useQuery, useState } from '@apollo/client';
 // import Login from '../../components/home/Login/Login'
@@ -8,15 +10,17 @@ import UserNav from "./UserNav";
 // import { QUERY_USER } from '../../utils/queries'
 // import { useMainContext } from '../../utils/GlobalState'
 
-
 const Header = (props) => {
-
-    return (
-        <div>
-        <h1>This Is User Header. It wraps UserNav, sending on its props</h1>
-        <UserNav {...props} />
+  return (
+    <div className="header-container">
+      <div className="header">
+        <h1 className="h1header">Streamline</h1>
+        <div className="nav-div">
+          <UserNav {...props} className="nav-container" />
         </div>
-    );
-  };
-  
-  export default Header;
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- client/src/components/user/UserNav.js
+++ client/src/components/user/UserNav.js
@@ -1,8 +1,14 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
 import { Navbar, Nav } from "rsuite";
 import HomeIcon from "@rsuite/icons/legacy/Home";
+import { FaTicketAlt } from "react-icons/fa";
+import { FaCalendarAlt } from "react-icons/fa";
+import { BsFillPersonFill } from "react-icons/bs";
+import { ImVideoCamera } from "react-icons/im";
+
 import "./user.css";
 // import { useQuery, useState } from '@apollo/client';
 // import Login from '../../components/home/Login/Login'
@@ -10,50 +16,57 @@ import "./user.css";
 // import { QUERY_USER } from '../../utils/queries'
 // import { useMainContext } from '../../utils/GlobalState'
 
-const UserNav = ({ user }) => {
+const CustomUserNav = ({ user, onSelect, activeKey, ...props }) => {
   return (
-    <Navbar>
-      <Navbar.Brand href="#">
-        <Link to="/UserHome">
-          <p>STREAMLINE</p>
-        </Link>
-      </Navbar.Brand>
+    <Navbar {...props} className="navbar">
       <Nav>
-        <Nav.Item icon={<HomeIcon />}>
-          <Link to="/UserHome">
+        <Nav.Item icon={<HomeIcon />} className="nav">
+          <Link to="/UserHome" className="nav-item">
             <p>HOME</p>
           </Link>
         </Nav.Item>
-        <Nav.Item>
-          <Link to="/Tickets">
+        <Nav.Item icon={<FaTicketAlt />}>
+          <Link to="/Tickets" className="nav-item">
             <p>Tickets</p>
           </Link>
         </Nav.Item>
-        <Nav.Item>
-          <Link to="/Profile">
+        <Nav.Item icon={<BsFillPersonFill />}>
+          <Link to="/Profile" className="nav-item">
             <p>Profile</p>
           </Link>
         </Nav.Item>
-        <Nav.Item>
-          {" "}
-          <Link to="/Livestream">
+        <Nav.Item icon={<ImVideoCamera />}>
+          <Link to="/Livestream" className="nav-item">
             <p>Livestream</p>
           </Link>
         </Nav.Item>
-        <Nav.Menu title="Events">
+        <Nav.Menu title="Events" icon={<FaCalendarAlt />} className="nav-item">
           <Nav.Item>
-            <Link to="/UpcomingEvents">
+            <Link to="/UpcomingEvents" className="subnav-item">
               <p>Upcoming Events</p>
             </Link>
           </Nav.Item>
           <Nav.Item>
-            <Link to="/PastEvents">
+            <Link to="/PastEvents" className="subnav-item">
               <p>Past Events</p>
             </Link>
           </Nav.Item>
         </Nav.Menu>
       </Nav>
     </Navbar>
+  );
+};
+const UserNav = () => {
+  const [activeKey, setActiveKey] = React.useState(null);
+
+  return (
+    <>
+      <CustomUserNav
+        appearance="inverse"
+        activeKey={activeKey}
+        onSelect={setActiveKey}
+      />
+    </>
   );
 };
 
