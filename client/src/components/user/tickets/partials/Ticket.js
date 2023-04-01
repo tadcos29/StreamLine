9c37eb873ec3ffe63274b2827fcb5f1aadeea5e3
tadcos29 <taddeo.costanzo@alumni.utoronto.ca>
2023-03-31 23:30:13 -0400

    Ticket page partially implemented

--- client/src/components/user/tickets/partials/Ticket.js
+++ client/src/components/user/tickets/partials/Ticket.js
@@ -1,16 +1,24 @@
 import React from "react";
 import Purchase from "./Purchase";
-
+import { useMutation, useQuery } from '@apollo/client';
+import { QUERY_EVENT, QUERY_TICKET } from "../../../../utils/queries";
 const Ticket = ({ ticket }) => {
-  console.log(ticket);
+  // const { loading, error, data } = useQuery(QUERY_TICKET, {
+  //   variables: { _id: ticket.event},
+  // });
+  // if (loading) return <p>Loading Ticket...</p>;
+  // if (error) return <p>Error: {error.message}</p>;
+
+  // console.log(ticket);
+  
   return (
     <div className="ticket-card">
     
       <h3>{ticket._id}</h3>
       <h3>it's a ticket</h3>
-      <p>{ticket.description}</p>
+      {/* <p>{data.description}</p> */}
       <a href={ticket.url}>Video Link</a>
-      <Purchase ticket={ticket} />
+      {/* <Purchase ticket={ticket} /> */}
     </div>
   );
 };
