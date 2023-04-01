e139a3dec06f2e5cd1900bde70548d25b8b2ef0c
Jacob Postill <postilljacob@gmail.com>
2023-04-01 17:51:14 -0400

    livestream general functionality

--- client/src/components/user/tickets/partials/Ticket.js
+++ client/src/components/user/tickets/partials/Ticket.js
@@ -2,6 +2,8 @@ import React from "react";
 import Purchase from "./Purchase";
 import { useMutation, useQuery } from '@apollo/client';
 import { QUERY_EVENT, QUERY_TICKET } from "../../../../utils/queries";
+import { Link, NavLink } from "react-router-dom";
+
 const Ticket = ({ ticket }) => {
   // const { loading, error, data } = useQuery(QUERY_TICKET, {
   //   variables: { _id: ticket.event},
@@ -10,15 +12,22 @@ const Ticket = ({ ticket }) => {
   // if (error) return <p>Error: {error.message}</p>;
 
   console.log(ticket);
-  
+
+  const handleLinkClick = (event) => {
+    const linkUrl = event.target.getAttribute('data-url');
+    console.log(linkUrl);
+    localStorage.setItem('linkurl', linkUrl);
+  };
+
   return (
     <div className="ticket-card">
     
       <h5>Your exclusive ticket for...</h5>
       <h2><b>{ticket.event.name}</b></h2>
       <p>You purchased it on {ticket.purchaseDate}. The broadcast date is {ticket.event.streamTime}</p>
-      <a href={ticket.url}>Video Link</a>
-      {/* <Purchase ticket={ticket} /> */}
+      <Link to="/Livestream" className='link' id='link' onClick={handleLinkClick} data-url={ticket.event.url}>
+        View Livestream{ticket.event.url}
+      </Link>      {/* <Purchase ticket={ticket} /> */}
     </div>
   );
 };
